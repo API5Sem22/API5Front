50e422800639a1dfbf552fd276180737143d5a5b
Mike B <mikebbatista@hotmail.com>
2022-04-04 20:01:35 -0300

    criando tela de cadastro dos users

---
+++ dw/src/views/UserRegister.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>Cadastrar Usuário</h2>
+    <v-combobox
+    dense
+>   </v-combobox>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        select: ['Vuetify', 'Programming'],
+        items: [
+          'Programming',
+          'Design',
+          'Vue',
+          'Vuetify',
+        ],
+      }
+    },
+  }
+</script>
